9a286169214788adb64882481fde6afaf4a94555
douglasshiro <douglasshiro@hotmail.com>
2018-05-14 12:39:06 -0300

    inicio construcao menu

--- main.c
+++ main.c
@@ -4,6 +4,8 @@
 //
 
 #include <stdio.h>
+#include <string.h>
+#include "ref.h"
 
 typedef enum _bool {
     false = 0,
@@ -13,35 +15,63 @@ typedef enum _bool {
 int procurarTabela91(int);
 char tabela[91] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!' , '#' , '$' , '>' , '?' , '@' , '[' , ']' , '^' , '_' , '`' , '{' , '|' , '}' , '~' , '"' , '%' , '&' , '(' , ')' , '*' , '+' , ',' , '.' , '/' , ':' , ';' , '<' , '='};
 
-int main(int argc, const char * argv[]) {
-    
-    int c;
+int main(int argc, char * argv[]) {
+
+    int c, escolha;
     FILE *file;
-    file = fopen("teste.txt", "r");
-    
-    if (file) {
-        int bufferBase91 = 0;
-        int y1 = 0, y2 = 0;
-        while ((c = getc(file)) != EOF){
-            putchar(c);
-            if (bufferBase91 == 0) {
-                //estamos preenchendo o y1 de um novo bloco
-                y1 = procurarTabela91(c);
-                //printf(": %d; ", y1);
-                bufferBase91++;
-            }
-            else {
-                //estamos preenchendo o y2 de um novo bloco
-                y2 = procurarTabela91(c);
-                printf("; y1: %d, y2: %d; ", y1, y2);
-                printf("%d\n", (y1 * 91 + y2));
-                bufferBase91 = 0;
-            }
-        }
-        fclose(file);
-    }
-    
-    
+	char* instrucao = argv[1];
+	char* filename = argv[2];
+
+	if ( argc == 1) {
+		do {
+			printf("Programa de codificacao e decodificacao base91\n");
+			printf("Escolha a opcao desejada:\n");
+			printf("1.Codificar arquivo texto para arquivo binario base91;\n");
+			printf("2.Decodificar arquivo binario para base91;\n");
+			printf("3.Sair do programa;\n");
+			scanf("%d", &escolha);
+		} while (escolha != 3);
+		if (escolha == 1) {
+
+		} else if (escolha == 2) {
+
+		} else if (escolha == 3) {
+
+		}
+
+	}
+
+    if (argc != 3) {
+		printf("Erro: numero de parametros para o programa incorreto\n");
+	} else {
+		file = fopen(filename, "r");
+		if (!file) {
+			printf("Erro: problema para encontrar arquivo\n");
+		} else {
+
+	        int bufferBase91 = 0;
+	        int y1 = 0, y2 = 0;
+	        while ((c = getc(file)) != EOF){
+	            putchar(c);
+	            if (bufferBase91 == 0) {
+	                //estamos preenchendo o y1 de um novo bloco
+	                y1 = procurarTabela91(c);
+	                //printf(": %d; ", y1);
+	                bufferBase91++;
+	            }
+	            else {
+	                //estamos preenchendo o y2 de um novo bloco
+	                y2 = procurarTabela91(c);
+	                printf("; y1: %d, y2: %d; ", y1, y2);
+	                printf("%d\n", (y1 * 91 + y2));
+	                bufferBase91 = 0;
+	            }
+	        }
+	        fclose(file);
+	    }
+	}
+
+
     return 0;
 }
 
@@ -72,7 +102,7 @@ int procurarTabela91(int ascii) {
         //procurar por caracter especial
         procurarCaracterEspecial = true;
     }
-    
+
     if (procurarCaracterEspecial) {
         for (int i = 62; i < 91; i++) {
             if (tabela[i] == ascii) {
@@ -81,7 +111,7 @@ int procurarTabela91(int ascii) {
             }
         }
     }
-    
+
     //nao encontrado
     return -1;
 }
